f2f98ae0b102378c821c3567d39fa547ad60e2ab
xiaowei.song <songxiaowei@chinagoods.com>
2022-06-10 10:15:09 +0800

    #Updates#回调方法更新

--- app/app/flaskr/cb/view.py
+++ app/app/flaskr/cb/view.py
@@ -8,9 +8,10 @@ from flask import Blueprint
 
 from app.flaskr.cb.bu import get_data
 
-cb_bp = Blueprint('cb', __name__, url_prefix='/api/cb')
+cb_bp = Blueprint('cb', __name__)
 
 
-@cb_bp.route('/adv_track', methods=('GET', 'POST'))
+@cb_bp.route('/ghost/api/cb/adv_track', methods=('GET', 'POST'))
+@cb_bp.route('/api/cb/adv_track', methods=('GET', 'POST'))
 def ad_track():
     return get_data()
