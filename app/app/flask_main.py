72985c27699b0b77ea75c04a8ff67bf39f82c48a
xiaowei.song <songxiaowei@chinagoods.com>
2023-03-02 13:42:38 +0800

    #Updates#应用日志不输出到控制台

--- app/app/flask_main.py
+++ app/app/flask_main.py
@@ -463,11 +463,11 @@ def configure_logging(app):
         app.logger.addHandler(error_file_handler)
 
         # 线上环境输出错误日志到控制台
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(level="WARNING")
-        console_format = logging.Formatter(fmt=app.config['ERROR_FORMATTER'])
-        console_handler.setFormatter(fmt=console_format)
-        app.logger.addHandler(console_handler)
+        # console_handler = logging.StreamHandler()
+        # console_handler.setLevel(level="WARNING")
+        # console_format = logging.Formatter(fmt=app.config['ERROR_FORMATTER'])
+        # console_handler.setFormatter(fmt=console_format)
+        # app.logger.addHandler(console_handler)
 
     # 设置系统默认日志记录等级
     app.logger.setLevel(logging.INFO)
